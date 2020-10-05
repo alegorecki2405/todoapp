3971b56bd92106818992135425c21ec26d04bfa5
Aleksander Gorecki <alegorecki@gmail.com>
2020-10-05 21:50:37 +0200

    lesson 3

--- src/main/java/olek/gorecki/todoapp/model/Task.java
+++ src/main/java/olek/gorecki/todoapp/model/Task.java
@@ -1,14 +1,13 @@
 package olek.gorecki.todoapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String description;
     private boolean done;
