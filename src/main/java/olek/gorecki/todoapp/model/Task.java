f848479bbcb1c442b25c45dfb7c624ff99b652af
Aleksander Gorecki <alegorecki@gmail.com>
2020-10-18 23:31:25 +0200

    lesson 4

--- src/main/java/olek/gorecki/todoapp/model/Task.java
+++ src/main/java/olek/gorecki/todoapp/model/Task.java
@@ -1,16 +1,22 @@
 package olek.gorecki.todoapp.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @NotBlank(message = "Task's description must not be empty")
     private String description;
     private boolean done;
+
+    public Task() {
+    }
 
     public int getId() {
         return id;
