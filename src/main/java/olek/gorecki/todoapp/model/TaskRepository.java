41a98b6f36a9be8ffa5d8eabda5ffc94b3f6f4ab
Aleksander Gorecki <alegorecki@gmail.com>
2020-11-01 00:08:11 +0100

    lesson 5

--- src/main/java/olek/gorecki/todoapp/model/TaskRepository.java
+++ src/main/java/olek/gorecki/todoapp/model/TaskRepository.java
@@ -2,8 +2,15 @@ package olek.gorecki.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "todos", collectionResourceRel = "todos")
 interface TaskRepository extends JpaRepository<Task, Integer> {
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
 
+    @Override
+    @RestResource(exported = false)
+    void delete(Task task);
 }
