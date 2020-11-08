8558376f6516fe105cedd4591a7f9d62a0f3b777
Aleksander Gorecki <alegorecki@gmail.com>
2020-11-08 22:20:33 +0100

    lesson 7

--- src/main/java/olek/gorecki/todoapp/controller/TaskController.java
+++ src/main/java/olek/gorecki/todoapp/controller/TaskController.java
@@ -1,10 +1,15 @@
 package olek.gorecki.todoapp.controller;
 
+import olek.gorecki.todoapp.model.Task;
 import olek.gorecki.todoapp.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 
 @RepositoryRestController
 public class TaskController {
@@ -15,5 +20,15 @@ public class TaskController {
         this.repository = repository;
     }
 
+    @GetMapping(path = "/tasks", params = {"!sort","!page","!size"})
+    ResponseEntity<List<Task>> readAllTasks() {
+        logger.warn("Exposing all the tasks");
+        return ResponseEntity.ok(repository.findAll());
+    }
 
+    @GetMapping(path = "/tasks")
+    ResponseEntity<List<Task>> readAllTasks(Pageable page) {
+        logger.info("Custom pageable");
+        return ResponseEntity.ok(repository.findAll(page).getContent());
+    }
 }
