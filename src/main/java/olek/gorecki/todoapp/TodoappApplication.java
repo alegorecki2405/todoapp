6da21900418c69cb248d8b3574d91d2d969f4cef
Aleksander Gorecki <alegorecki@gmail.com>
2020-11-13 15:36:29 +0100

    lesson 8

--- src/main/java/olek/gorecki/todoapp/TodoappApplication.java
+++ src/main/java/olek/gorecki/todoapp/TodoappApplication.java
@@ -3,26 +3,20 @@ package olek.gorecki.todoapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import javax.validation.Validator;
+
 @SpringBootApplication
-public class TodoappApplication implements RepositoryRestConfigurer {
+public class TodoappApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(TodoappApplication.class, args);
     }
 
     @Bean
-    LocalValidatorFactoryBean validator(){
+    Validator validator(){
         return new LocalValidatorFactoryBean();
-    }
-
-    @Override
-    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
-        validatingListener.addValidator("beforeCreate", validator());
-        validatingListener.addValidator("beforeSave", validator());
     }
 
 }
