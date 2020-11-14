dc9dcd0d41d512c25bfc0deb478399f29a3414c1
Aleksander Gorecki <alegorecki@gmail.com>
2020-11-14 21:58:38 +0100

    lesson 9

--- src/main/java/olek/gorecki/todoapp/model/TaskRepository.java
+++ src/main/java/olek/gorecki/todoapp/model/TaskRepository.java
@@ -1,9 +1,23 @@
 package olek.gorecki.todoapp.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+
+public interface TaskRepository {
+    List<Task> findAll();
+
+    Optional<Task> findById(Integer i);
+
+    Task save(Task entity);
+
+    Page<Task> findAll(Pageable page);
+
+    List<Task> findByDone(@Param("state") boolean done);
+
+    boolean existsById(Integer id);
 }
